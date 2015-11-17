ad5ed5e95e4a58dc0089bd46ec715d713c92dc9d
Sobin George Thomas <sobingt@gmail.com>
2015-11-17 17:16:52 +0530

    remove from cart

--- js/main.js
+++ js/main.js
@@ -2,22 +2,48 @@
 var cart = function() {
     this.items = [];
     this.price = 0;
+    this.removeFromCart = function(index) {
+      this.price-=this.items[index].price;
+      this.items.splice(index, 1);
+      this.render();
+    };
     this.removeFromCartById = function(id) {
         for (var i = 0; i < this.items.length; i++) {
             if (this.items[i].id == id)
+              {
+                this.price-=this.items[i].price;
                 this.items.splice(i, 1);
+              }
+                
         }
     };
     this.removeFromCartByName = function(name) {
         for (var i = 0; i < this.items.length; i++) {
             if (this.items[i].name == name)
+              {
+                this.price-=this.items[i].price;
                 this.items.splice(i, 1);
+              }
         }
     };
     this.addToCart = function(product) {
         this.items.push(product);
         this.price += product.price;
     };
+    this.render = function(){
+      document.getElementById('cart').innerHTML ='';
+      document.getElementById('cart').innerHTML = '<div>Total price <span class="price">' + this.price + '</span></div><div>Total items <span class="cartitems">' + this.items.length + '</span></div>';
+    for (var i = 0; i < this.items.length; i++) {
+        var cartItemDiv = document.createElement('div');
+        cartItemDiv.className = 'cartItem';
+        cartItemDiv.setAttribute('id', 'cartItem[' + i + ']');
+        cartItemDiv.setAttribute('data-id', this.items[i].id);
+        cartItemDiv.innerHTML = '<span class="productName">' + this.items[i].name + '</span><span class="productPrice">' + this.items[i].price + '</span><span class="remove" onclick="cartObject.removeFromCart('+i+')">X</span>';
+        document.getElementById('cart').appendChild(cartItemDiv);
+
+    }
+      
+    }
 };
 //cart object template
 cartObject = new cart();
@@ -42,6 +68,7 @@ var init = function() {
     products[2] = new product(3, "Lenovo", "Lenovo 128gb SSD 4GB RAM", 2500);
     products[3] = new product(4, "HP", "HP 128gb SSD 4GB RAM", 1200);
     cartObject.addToCart(products[0]);
+    cartObject.addToCart(products[1]);
     for (var i = 0; i < products.length; i++) {
         div = document.createElement('div');
         div.className = 'product';
@@ -53,13 +80,14 @@ var init = function() {
 
     div = document.createElement('div');
     div.className = 'cart';
+    div.setAttribute('id', 'cart');
     div.innerHTML = '<div>Total price <span class="price">' + cartObject.price + '</span></div><div>Total items <span class="cartitems">' + cartObject.items.length + '</span></div>';
     for (var i = 0; i < cartObject.items.length; i++) {
         var cartItemDiv = document.createElement('div');
         cartItemDiv.className = 'cartItem';
         cartItemDiv.setAttribute('id', 'cartItem[' + i + ']');
         cartItemDiv.setAttribute('data-id', cartObject.items[i].id);
-        cartItemDiv.innerHTML = '<span class="productName">' + cartObject.items[i].name + '</span><span class="productPrice">' + cartObject.items[i].price + '</span><span class="remove" onclick="">X</span>';
+        cartItemDiv.innerHTML = '<span class="productName">' + cartObject.items[i].name + '</span><span class="productPrice">' + cartObject.items[i].price + '</span><span class="remove" onclick="cartObject.removeFromCart('+i+')">X</span>';
         div.appendChild(cartItemDiv);
 
     }
